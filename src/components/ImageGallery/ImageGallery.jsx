a34a92e0106ae23da8c917a45047d4ff1309d8c1
yeah <154704827+Ternivka@users.noreply.github.com>
2025-01-14 07:13:19 +0100

    fixed

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,20 @@
 import ImageCard from "../ImageCard/ImageCard";
 import s from "./ImageGallery.module.css";
 
-const Images = ({ image, page, onImageClick }) => {
+const ImageGallery = ({ images, onImageClick }) => {
   return (
     <div>
       <ul className={s.container}>
-        {image.map((item, index) => {
-          const uniqueKey = `${item.id}-${page}-${index}`;
-          return (
-            <li key={uniqueKey}>
-              <div>
-                <ImageCard item={item} onImageClick={onImageClick} />
-              </div>
-            </li>
-          );
-        })}
+        {images.map((item) => (
+          <li key={item.id}>
+            <div>
+              <ImageCard item={item} onImageClick={onImageClick} />
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
 };
 
-export default Images;
+export default ImageGallery;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 import s from "./SearchBar.module.css";
+
 export const SearchBar = ({ onSearchChanged }) => {
   const [value, setValue] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!value.trim()) {
+      toast.error("Please enter a search query!");
+      return;
+    }
     onSearchChanged(value);
     console.log(value);
   };
@@ -22,7 +28,7 @@ export const SearchBar = ({ onSearchChanged }) => {
             placeholder="Search"
             className={s.input}
           />
-          <button className={s.btn}>
+          <button className={s.btn} type="submit">
             <FaSearch className={s.icon} />
           </button>
         </div>
